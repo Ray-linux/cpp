893cc49a7fdf51f38e374d2a7b33fe3659d2b7de
DeadX <ray852097@gmail.com>
2021-11-08 23:10:44 +0530

    Array in c++

---
+++ array1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int array[n];
+
+    for(int i=0; i<n; i++){
+        cin>>array[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+    
+    // cout<<array[3]<<endl;
+    return 0;
+}   
